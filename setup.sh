6b05117e9131f5771c73b2360cf4b14655768db6
Jérôme Pogeant <p-jerome@hotmail.fr>
2018-06-29 00:07:28 +0200

    Add PHP and composer installations

--- setup.sh
+++ setup.sh
@@ -12,7 +12,11 @@ sudo -s <<EOF
       ca-certificates \
       software-properties-common \
       xclip \
-      vim
+      vim \
+      php-cli \
+      php-mbstring \
+      php-xml \
+      unzip
 
     # Docker
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -31,6 +35,13 @@ sudo -s <<EOF
     curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 EOF
+
+# Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo -s mv composer.phar /usr/local/bin/composer
 
 # Git setup
 git config --global alias.co checkout
